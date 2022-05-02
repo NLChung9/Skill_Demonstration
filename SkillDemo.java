c83898ee40d1cea3c74503d51163f482868826b8
NLChung9 <88344454+NLChung9@users.noreply.github.com>
2022-05-01 22:42:55 -0700

    Added new Files

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,5 +1,8 @@
 public class SkillDemo{
     //Continue in VSCode
     //SkillDemo.multiply()
-    //public static int multiply(){
+    
+    public static int multiply(int num1, int num2){
+        return num1*num2;
+    }
 }
